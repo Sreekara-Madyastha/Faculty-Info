9c2ab947fe379b1fb7e9fdb30b916231f59bbda1
Sreekara-Madyastha <cse200001031@iiti.ac.in>
2021-10-12 20:24:24 +0530

    converted all into one server

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
-# 200001031
-from flask import Flask,render_template,redirect,request,session,jsonify
+
+import re
+from flask import Flask, render_template, request, redirect, session, jsonify
 from flask.scaffold import F
 from flask_session import Session
 import mysql.connector
+
 app = Flask(__name__)
 mydb = mysql.connector.connect(
   host="localhost",
@@ -11,22 +13,23 @@ mydb = mysql.connector.connect(
   database="courses_faculty"
 )
 mycursor=mydb.cursor()
-
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-sem=[]
+
+def my_function(x):
+  return list(dict.fromkeys(x))
+
+mycursor.execute('SELECT * FROM departments')
+departments=mycursor.fetchall()
+mycursor.execute('SELECT Semester FROM taughtby')
+sem=mycursor.fetchall()
+sem = my_function(sem)
+mycursor.execute('SELECT * from faculty')
+fac=mycursor.fetchall()
 
 @app.route("/")
 @app.route("/home")
 def begin():
-    mycursor.execute('SELECT * FROM departments')
-    departments=mycursor.fetchall()
-    mycursor.execute('SELECT Semester FROM taughtby')
-    global sem
-    sem=mycursor.fetchall()
-    sem = my_function(sem)
-    return render_template("home.html",departments=departments,sem=sem)
+    return render_template("home.html",departments=departments,fac=fac,sem=sem)
 
 
 
@@ -70,34 +73,49 @@ def signup():
 def logout():
   session["userID"] = None
   return redirect("/login")
-DId=""
-def my_function(x):
-  return list(dict.fromkeys(x))
+# DId=""
 
-@app.route('/coursesearch',methods=["GET","POST"])
-def coursesearch():
-    global DId 
-    DId= request.form.get('depselect')
-    res=""
-    mycursor.execute('SELECT taughtby.CId,courses.CName,taughtby.FId,faculty.FName,taughtby.Semester,taughtby.NumStudents FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE courses.DId=%s',(DId,))
-    res=mycursor.fetchall()
-    mycursor.execute('SELECT taughtby.FId,faculty.FName FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE courses.DId=%s',(DId,))
-    fac=mycursor.fetchall()
-    fac = my_function(fac)
-    return render_template('home.html',res=res,DId=DId,fac=fac,sem=sem)
-FId=""
-@app.route('/facultysearch',methods=["GET","POST"])
-def facsearch():
-  global FId
-  FId=request.form.get('facultysel')
-  mycursor.execute('SELECT * FROM faculty WHERE FId=%s',(FId,))
-  facres=mycursor.fetchall()
-  return render_template('home.html',facres=facres,FId=FId,DId=DId,sem=sem)
-semsearch=""
-@app.route('/semsearch',methods=["GET","POST"])
-def semsearch():
-  global semsearch
-  semsearch=request.form.get('semsearch')
-  mycursor.execute('SELECT taughtby.CId,courses.CName,taughtby.FId,faculty.FName,taughtby.NumStudents FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE taughtby.Semester=%s AND taughtby.FId LIKE %s',(semsearch,FId,))
-  courses=mycursor.fetchall()
-  return render_template('home.html',courses=courses,FId=FId,sem=sem,Sem=semsearch,DId=DId)
+
+# @app.route('/coursesearch',methods=["GET","POST"])
+# def coursesearch():
+#     global DId 
+#     DId= request.form.get('depselect')
+#     res=""
+#     mycursor.execute('SELECT taughtby.CId,courses.CName,taughtby.FId,faculty.FName,taughtby.Semester,taughtby.NumStudents FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE courses.DId=%s',(DId,))
+#     res=mycursor.fetchall()
+#     mycursor.execute('SELECT taughtby.FId,faculty.FName FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE courses.DId=%s',(DId,))
+#     fac=mycursor.fetchall()
+#     fac = my_function(fac)
+#     return render_template('home.html',res=res,DId=DId,fac=fac,sem=sem)
+# FId=""
+# @app.route('/facultysearch',methods=["GET","POST"])
+# def facsearch():
+#   FId=request.form.get('facultysel')
+#   mycursor.execute('SELECT taughtby.CId,courses.CName,taughtby.FId,faculty.FName,taughtby.Semester,taughtby.NumStudents FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE( courses.DId=%s AND taughtby.FId=%s)',(DId,FId,))
+#   facres=mycursor.fetchall()
+#   return render_template('home.html',facres=facres,FId=FId,DId=DId,sem=sem)
+# semsearch=""
+# @app.route('/semsearch',methods=["GET","POST"])
+# def semsearch():
+#   global semsearch
+#   semsearch=request.form.get('semsearch')
+#   mycursor.execute('SELECT taughtby.CId,courses.CName,taughtby.FId,faculty.FName,taughtby.NumStudents FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE taughtby.Semester=%s AND taughtby.FId LIKE %s',(semsearch,FId,))
+#   courses=mycursor.fetchall()
+#   return render_template('home.html',courses=courses,FId=FId,sem=sem,Sem=semsearch,DId=DId)
+        
+@app.route('/search',methods=["GET","POST"])
+def search():
+  if request.form.get('facselect')!="" :
+    mycursor.execute('SELECT taughtby.CId,courses.CName,taughtby.FId,faculty.FName,taughtby.Semester,taughtby.NumStudents FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE( courses.DId LIKE %s AND taughtby.FId LIKE %s AND taughtby.Semester LIKE %s)',('%'+request.form.get('depselect')+'%',request.form.get('facselect'),'%'+request.form.get('semsearch')+'%'))
+    result=mycursor.fetchall()
+  else:
+    mycursor.execute('SELECT taughtby.CId,courses.CName,taughtby.FId,faculty.FName,taughtby.Semester,taughtby.NumStudents FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE( courses.DId LIKE %s AND taughtby.FId LIKE %s AND taughtby.Semester LIKE %s)',('%'+request.form.get('depselect')+'%',"%",'%'+request.form.get('semsearch')+'%'))
+    result=mycursor.fetchall()
+  # print(result)
+  return render_template('home.html',result=result,FId=request.form.get('facselect'),DId=request.form.get('depselect'),semester=request.form.get('semsearch'),departments=departments,fac=fac,sem=sem)
+
+
+  
+
+if __name__ == '__main__':
+  app.run(debug=True)
