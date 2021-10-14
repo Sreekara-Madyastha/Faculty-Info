9676a4f6633e6dd7811f609b184c1d62d9438426
Spartan-1-1-7 <92378861+Spartan-1-1-7@users.noreply.github.com>
2021-10-14 12:21:09 +0530

    Update application.py
    Added login-signup feature for authorizedPersonell

--- application.py
+++ application.py
@@ -1,19 +1,16 @@
-
-import re
-from flask import Flask, render_template, request, redirect, session, jsonify
-from flask.scaffold import F
-from flask_session import Session
+from flask import Flask, render_template, request, redirect, session
 import mysql.connector
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(10)
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="Sreek@r123",
+  password="IamIRONmanMYSQL",
   database="courses_faculty"
 )
 mycursor=mydb.cursor()
-Session(app)
 
 def my_function(x):
   return list(dict.fromkeys(x))
@@ -31,48 +28,108 @@ fac=mycursor.fetchall()
 def begin():
     return render_template("home.html",departments=departments,fac=fac,sem=sem)
 
+       
+@app.route('/search',methods=["GET","POST"])
+def search():
+  if request.form.get('facselect')!="" :
+    mycursor.execute('SELECT taughtby.CId,courses.CName,taughtby.FId,faculty.FName,taughtby.Semester,taughtby.NumStudents FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE( courses.DId LIKE %s AND taughtby.FId LIKE %s AND taughtby.Semester LIKE %s)',('%'+request.form.get('depselect')+'%',request.form.get('facselect'),'%'+request.form.get('semsearch')+'%'))
+    result=mycursor.fetchall()
+  else:
+    mycursor.execute('SELECT taughtby.CId,courses.CName,taughtby.FId,faculty.FName,taughtby.Semester,taughtby.NumStudents FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE( courses.DId LIKE %s AND taughtby.FId LIKE %s AND taughtby.Semester LIKE %s)',('%'+request.form.get('depselect')+'%',"%",'%'+request.form.get('semsearch')+'%'))
+    result=mycursor.fetchall()
+  # print(result)
+  return render_template('home.html',result=result,FId=request.form.get('facselect'),DId=request.form.get('depselect'),semester=request.form.get('semsearch'),departments=departments,fac=fac,sem=sem)
 
 
-@app.route("/login",methods=["GET","POST"])
+@app.route('/login')
 def login():
-  if request.method == "POST" :
-    userID=request.form.get("userID")
-    password=request.form.get("password")
-    mycursor.execute("SELECT * FROM Login_data WHERE userID=%s AND password=%s",(userID,password))
-    myvalue=mycursor.fetchone()
-    if bool(myvalue) is True:
-      session["userID"]=userID
-      return redirect("/begin")
-    else :
-      aler="userID/Password is incorrect! Try again"
-      return render_template("login.html",aler=aler)
-  return render_template("login.html")
+    return render_template('login.html')
 
 
-@app.route("/signup",methods=["GET","POST"])
-def signup():
-  if request.method == "POST" :
-    userID = request.form.get("userID")
-    password = request.form.get("password")
-    phno = request.form.get("phno")
-    mycursor.execute("SELECT * FROM Login_data WHERE userID=%s",(userID,))
-    myvalue=mycursor.fetchone()
-    if bool(myvalue) is False:
-      sql = "INSERT INTO Login_data (userID, phno,password) VALUES (%s, %s,%s)"
-      val = (userID, phno,password)
-      mycursor.execute(sql, val)
-      mydb.commit()
-      session["userID"]=userID
-      return redirect("/begin")
-    else :
-      aler="userID is already taken Try something different or login into your existing account"
-      return render_template("signup.html",aler=aler)
-  return render_template("signup.html")
+@app.route('/login_validation', methods=['POST'])
+def login_validation():
+    Username = request.form.get('Username')
+    password = request.form.get('password')
 
-@app.route("/logout",methods=["GET","POST"])
-def logout():
-  session["userID"] = None
-  return redirect("/login")
+    mycursor.execute("""select * from `authorizedPersonell` where `Username` = '{}' and `Password` = '{}'""".format(Username, password))
+    users = mycursor.fetchall()
+
+    if len(users) > 0:
+        session['AId'] = users[0][0]
+        return redirect('/access')
+    else:
+        return render_template('login.html', error_message="Invalid Username or Password")
+
+
+@app.route('/access')
+def home():
+    if 'AId' in session:
+        return render_template('access.html')
+    else:
+        return redirect('/login')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    addUsername = request.form.get('reg_Username')
+    addEmail = request.form.get('reg_Email')
+    addPassword = request.form.get('reg_Password')
+
+    mycursor.execute("""insert into `authorizedPersonell` (`Username`, `Email`, `Password`) values
+             ('{}', '{}', '{}')""".format(addUsername, addEmail, addPassword))
+    mydb.commit()
+
+    return render_template('login.html', after_reg_message="Account created successfully! Login to proceed")
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
+
+# @app.route("/login",methods=["GET","POST"])
+# def login():
+#   if request.method == "POST" :
+#     userID=request.form.get("userID")
+#     password=request.form.get("password")
+#     mycursor.execute("SELECT * FROM Login_data WHERE userID=%s AND password=%s",(userID,password))
+#     myvalue=mycursor.fetchone()
+#     if bool(myvalue) is True:
+#       session["userID"]=userID
+#       return redirect("/begin")
+#     else :
+#       aler="userID/Password is incorrect! Try again"
+#       return render_template("login.html",aler=aler)
+#   return render_template("login.html")
+
+
+# @app.route("/signup",methods=["GET","POST"])
+# def signup():
+#   if request.method == "POST" :
+#     userID = request.form.get("userID")
+#     password = request.form.get("password")
+#     phno = request.form.get("phno")
+#     mycursor.execute("SELECT * FROM Login_data WHERE userID=%s",(userID,))
+#     myvalue=mycursor.fetchone()
+#     if bool(myvalue) is False:
+#       sql = "INSERT INTO Login_data (userID, phno,password) VALUES (%s, %s,%s)"
+#       val = (userID, phno,password)
+#       mycursor.execute(sql, val)
+#       mydb.commit()
+#       session["userID"]=userID
+#       return redirect("/begin")
+#     else :
+#       aler="userID is already taken Try something different or login into your existing account"
+#       return render_template("signup.html",aler=aler)
+#   return render_template("signup.html")
+
+# @app.route("/logout",methods=["GET","POST"])
+# def logout():
+#   session["userID"] = None
+#   return redirect("/login")
 # DId=""
 
 
@@ -102,20 +159,3 @@ def logout():
 #   mycursor.execute('SELECT taughtby.CId,courses.CName,taughtby.FId,faculty.FName,taughtby.NumStudents FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE taughtby.Semester=%s AND taughtby.FId LIKE %s',(semsearch,FId,))
 #   courses=mycursor.fetchall()
 #   return render_template('home.html',courses=courses,FId=FId,sem=sem,Sem=semsearch,DId=DId)
-        
-@app.route('/search',methods=["GET","POST"])
-def search():
-  if request.form.get('facselect')!="" :
-    mycursor.execute('SELECT taughtby.CId,courses.CName,taughtby.FId,faculty.FName,taughtby.Semester,taughtby.NumStudents FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE( courses.DId LIKE %s AND taughtby.FId LIKE %s AND taughtby.Semester LIKE %s)',('%'+request.form.get('depselect')+'%',request.form.get('facselect'),'%'+request.form.get('semsearch')+'%'))
-    result=mycursor.fetchall()
-  else:
-    mycursor.execute('SELECT taughtby.CId,courses.CName,taughtby.FId,faculty.FName,taughtby.Semester,taughtby.NumStudents FROM ((taughtby INNER JOIN courses ON courses.CId=taughtby.CId) INNER JOIN faculty ON taughtby.FId=faculty.FId) WHERE( courses.DId LIKE %s AND taughtby.FId LIKE %s AND taughtby.Semester LIKE %s)',('%'+request.form.get('depselect')+'%',"%",'%'+request.form.get('semsearch')+'%'))
-    result=mycursor.fetchall()
-  # print(result)
-  return render_template('home.html',result=result,FId=request.form.get('facselect'),DId=request.form.get('depselect'),semester=request.form.get('semsearch'),departments=departments,fac=fac,sem=sem)
-
-
-  
-
-if __name__ == '__main__':
-  app.run(debug=True)
